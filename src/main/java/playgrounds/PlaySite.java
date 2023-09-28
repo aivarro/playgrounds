e59cda96808ae8e8fc3b420db52eded6e53ba221
Aivar Romandi <aivar.romandi@gmail.com>
2023-09-29 02:08:10 +0300

    play site utilization is now calculated

--- src/main/java/playgrounds/PlaySite.java
+++ src/main/java/playgrounds/PlaySite.java
@@ -1,12 +1,11 @@
 package playgrounds;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@AllArgsConstructor
+import static playgrounds.PlaySiteType.DOUBLE_SWINGS;
+
 @NoArgsConstructor
 @Data
 public class PlaySite {
@@ -16,4 +15,34 @@ public class PlaySite {
     private Integer capacity;
     private List<Kid> kidsOnSite;
     private List<Kid> kidsInQueue;
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Integer utilization;
+
+    public PlaySite(Long id, PlaySiteType type, Integer capacity, List<Kid> kidsOnSite, List<Kid> kidsInQueue) {
+        this.id = id;
+        this.type = type;
+        this.capacity = capacity;
+        this.kidsOnSite = kidsOnSite;
+        this.kidsInQueue = kidsInQueue;
+    }
+
+    public Integer getUtilization() {
+        if (kidsOnSite == null || kidsOnSite.isEmpty()) {
+            return 0;
+        } else {
+
+            int percentage = 100 * kidsOnSite.size() / capacity;
+
+            if (type.equals(DOUBLE_SWINGS)) {
+                if (kidsOnSite.size() == 1) {
+                    return 0;
+                } else {
+                    return percentage;
+                }
+            } else {
+                return percentage;
+            }
+        }
+    }
 }
